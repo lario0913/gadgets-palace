000afc3207d5eb3b02a2674b4893b6811a0ba6e8
Lario <osunkunleolanrewaju@gmail.com>
2020-12-23 16:21:42 +0100

    displaying the product on the screen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Products from './components/Products'
+import data from './data.json'
 
-function App() {
-  return (
-    <div className="container">
-      <header>Gadget Palace</header>
-      <main>Main</main>
-      <footer>All rights has been reserved</footer>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       products: data.products,
+       color: "",
+       sort: ""
+    }
+  }
+  
+  render() {
+    return (
+      <div className="container">
+        <header><a href='/'>Gadget Palace</a></header>
+        <main>
+          <div className='content'>
+            <div className='main'>
+              <Products products={this.state.products}/>
+            </div>
+            <div>Sidebar</div>
+          </div>
+        </main>
+        <footer>All rights has been reserved</footer>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
